9adcdb05aaa5ac6b5b3ef4889571ec1e79231407
Higor Nóbrega <higorst.nobrega@gmail.com>
2024-05-06 22:40:26 -0300

    Feature - Registered  models classes in admin

--- curso/admin.py
+++ curso/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Avaliacao, Curso
+
+
+@admin.register(Avaliacao)
+class AvaliacaoAdmin(admin.ModelAdmin):
+    list_display = ('curso', 'nome', 'email', 'comentario', 'criacao', 'atualizacao', 'ativo')
+
+admin.register(Curso)
+class CursoAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'url', 'criacao', 'atualizacao', 'ativo') 
+    
+
+
+
