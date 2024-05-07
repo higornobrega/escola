34303ebca6e8f2a4f0e451a9cc2efe84f4cb52b2
Higor Nóbrega <higorst.nobrega@gmail.com>
2024-05-06 23:23:29 -0300

    Feature - Created Curso and Avaliacao class in view

--- curso/views.py
+++ curso/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from .models import Avaliacao, Curso
+from .serializers import AvaliacaoSerializer, CursoSerialiser
+
+
+class CursoAPIView(APIView):
+    def get(self, request):
+        cursos = Curso.objects.all()
+        serializer = CursoSerialiser(cursos, many=True)
+        return Response(serializer.data)
+    
+class AvaliacaoAPIView(APIView):
+    def get(self, request):
+        avaliacoes = Avaliacao.objects.all()
+        serialiser = AvaliacaoSerializer(avaliacoes, many=True)
+        return Response(serialiser.data)
