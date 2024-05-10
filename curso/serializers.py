9003f3dacf9c29981f647dec342bafe750400930
Higor Nóbrega <higorst.nobrega@gmail.com>
2024-05-10 09:28:14 -0300

    Feature - Validation Created for avaliacao with a grade greater than 5 and return of cursos avaliacoes averages

--- curso/serializers.py
+++ curso/serializers.py
@@ -1,3 +1,4 @@
+from django.db.models import Avg
 from rest_framework import serializers
 
 from .models import Avaliacao, Curso
@@ -16,22 +17,31 @@ class AvaliacaoSerializer(serializers.ModelSerializer):
             'nome', 
             'email', 
             'comentario', 
+            'avaliacao',
             'criacao', 
             'atualizacao', 
             'ativo'
         )
         
+    def validate_avaliacao(self, valor):
+        if valor in range(1,6):
+            return valor
+        raise serializers.ValidationError('A nota da avaliação precisa ser entre 1 e 5')
+        
 class CursoSerialiser(serializers.ModelSerializer):
     
     # Relationship
     # 1. Nested Relationship:
-    # avaliacoes = AvaliacaoSerializer(many=True, read_only=True)
+    avaliacoes = AvaliacaoSerializer(many=True, read_only=True)
     
     # 2. HyperLinked Related Field:
     # avaliacoes = serializers.HyperlinkedRelatedField(many=True, read_only=True, view_name='avaliacao-detail')
     
     # 3. Primary key Related Field:
-    avaliacoes = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+    # avaliacoes = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+    
+    media_avaliacoes = serializers.SerializerMethodField()
+    
     class Meta:
         model = Curso
         fields = (
@@ -41,5 +51,11 @@ class CursoSerialiser(serializers.ModelSerializer):
             'criacao', 
             'atualizacao', 
             'ativo',
-            'avaliacoes'
+            'avaliacoes',
+            'media_avaliacoes'
         )
+    def get_media_avaliacoes(self, obj):
+        media = obj.avaliacoes.aggregate(Avg('avaliacao')).get('avaliacao__avg')
+        if media is None:
+            return 0
+        return round(media*2)/2
